e746439daf04ba321e6d5543966995181e3d035a
Luis Henrique Souza <mora0199@algonquinlive.com>
2019-11-10 20:19:34 -0500

    Updates

---
+++ precache-manifest.60857d11dc73a8f37e16c060718437f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd7b86ecbce04e8b63bd67f1fb3358c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "020a970dbd0854900717",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "63b743ebe729b0e6f0cd",
+    "url": "/static/css/main.63f0c5dc.chunk.css"
+  },
+  {
+    "revision": "020a970dbd0854900717",
+    "url": "/static/js/2.2283d53d.chunk.js"
+  },
+  {
+    "revision": "63b743ebe729b0e6f0cd",
+    "url": "/static/js/main.a4bd30a8.chunk.js"
+  },
+  {
+    "revision": "ec061f54c6854154b6e1",
+    "url": "/static/js/runtime-main.065bd361.js"
+  },
+  {
+    "revision": "129b27ae174ef2115724253d0cef91b5",
+    "url": "/static/media/favicon.129b27ae.png"
+  }
+]);
